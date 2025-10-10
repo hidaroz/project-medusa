6682590af9db8b787df4ee537ca29383e5495129
Hidar Elhassan <106721554+hidaroz@users.noreply.github.com>
2025-10-10 12:08:50 -0700

    Deploy latest Medusa webapp build to GitHub Pages

---
+++ project-medusa/_next/static/ChFHUcs2uomX0WPCOpg41/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/2bc8be4de45cc533.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/project-medusa//_next/:path+",
+        "destination": "/project-medusa/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ project-medusa/_next/static/chunks/a7267f2ed0151ae2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/59e980a188fcd186.js",
+  "static/chunks/db0dcf2bb24f6849.js",
+  "static/chunks/turbopack-ad777d9a06c3f7e9.js"
+])
